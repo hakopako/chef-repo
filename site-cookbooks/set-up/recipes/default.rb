d8e698aeff063767fd7f63a14072145a8e0dc94f
hakopako <hakohakohako.0518@gmail.com>
2014-04-18 18:44:40 +0900

    pearでのインストールを修正

--- site-cookbooks/set-up/recipes/default.rb
+++ site-cookbooks/set-up/recipes/default.rb
@@ -13,22 +13,22 @@ package "yum" do
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/epel-release-6-8.noarch.rpm" do
-  source "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-  not_if "rpm -qa | grep -q '^epel-release'"
-  action :create
-  notifies :install, "rpm_package[epel-release]", :immediately
+	source "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+	not_if "rpm -qa | grep -q '^epel-release'"
+	action :create
+	notifies :install, "rpm_package[epel-release]", :immediately
 end
 
 rpm_package "epel-release" do
-  source "#{Chef::Config[:file_cache_path]}/epel-release-6-8.noarch.rpm"
-  action :nothing
+	source "#{Chef::Config[:file_cache_path]}/epel-release-6-8.noarch.rpm"
+	action :nothing
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/remi-release-6.rpm" do
-  source "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
-  not_if "rpm -qa | grep -q '^remi-release'"
-  action :create
-  notifies :install, "rpm_package[remi-release]", :immediately
+	source "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
+	not_if "rpm -qa | grep -q '^remi-release'"
+	action :create
+	notifies :install, "rpm_package[remi-release]", :immediately
 end
 
 rpm_package "remi-release" do
@@ -60,6 +60,7 @@ end
 	php-devel 
 	php-mbstring 
 	php-mcrypt 
+	php-pear 
 }.each do |p|	
 	package p do
 		action :install
@@ -67,21 +68,36 @@ end
 	end
 end
 
+bash "upgrade_pear" do
+	user "root"
+	code <<-EOH
+		pear channel-discover pear.phpunit.de
+		pear channel-update pear.php.net
+		pear channel-discover components.ez.no  
+		pear upgrade pear  
+	EOH
+end
+
 bash "install_phpunit" do
-  user "root"
-  code "pear install -o pear.phpunit.de/PHPUnit; pear install HTTP_Request2; pear install --alldeps xml_serializer-beta;"
-  not_if { ::File.exists?("/usr/bin/phpunit")}
+	user "root"
+	code <<-EOH 
+		pear install -o pear.phpunit.de/PHPUnit 
+		pear install phpunit/PHP_CodeCoverage
+		pear install HTTP_Request2 
+		pear install --alldeps xml_serializer-beta
+	EOH
+	not_if { ::File.exists?("/usr/bin/phpunit")}
 end
 
 bash "install_jenkins" do
-  user "root"
-  code <<-EOH
-  yum -y install java-1.7.0-openjdk.x86_64
-  wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo 
-  rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
-  yum -y install jenkins
-  EOH
-  not_if { ::File.exists?("/usr/bin/jenkins")}
+	user "root"
+	code <<-EOH
+		yum -y install java-1.7.0-openjdk.x86_64
+		wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo 
+		rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+		yum -y install jenkins
+	EOH
+	not_if { ::File.exists?("/usr/bin/jenkins")}
 end
 
 ##################################################################
@@ -100,23 +116,23 @@ end
 ### templates
 ##################################################################
 template "php.ini" do
-    path "/etc/php.ini"
-    source "php.ini.erb"
-    mode 0644
+	path "/etc/php.ini"
+	source "php.ini.erb"
+	mode 0644
 end
 
 template "httpd.conf" do
-    path "/etc/httpd/conf/httpd.conf"
-    source "httpd.conf.erb"
-    mode 0644
+	path "/etc/httpd/conf/httpd.conf"
+	source "httpd.conf.erb"
+	mode 0644
 	notifies :start, 'service[httpd]'
 end
 
 template "iptables" do
-  	path "etc/sysconfig/iptables"
+	path "etc/sysconfig/iptables"
 	source "iptables"
 	owner "root"
- 	group "root"
+	group "root"
 	mode 0600
 	notifies :restart, 'service[iptables]'
 end
